90b8e05805f56e9c4d71272d016e0ef9a0ef3684
alexeev509 <32396885+alexeev509@users.noreply.github.com>
2021-05-04 00:37:02 +0300

    Update B2.cpp

--- SECOND_DAY/B2.cpp
+++ SECOND_DAY/B2.cpp
@@ -2,19 +2,25 @@
 #include "string"
 
 using namespace std;
-
+//https://silvertests.ru/GuideView.aspx?id=29833
 int n;
 
 void generate(int pos, int balance,string ans) {
+    //Если позиция равна n значит все ок;
     if(pos==n){
         cout<<ans<<"\n";
         return;
     }
+    //Ключевая идея в поддержке баланса;
+    //Если баланс позволяет добавить открывающуюся скобку - добавляем ее
     if (balance + 1 <= n - pos-1) {
         ans.push_back('(');
         generate(pos+1, balance+1,ans);
+        //Главное - не забыть убрать скобку которую добавили - ведь далее на ее место может встать противоположная скобка
+        //Я на этом спотыкался :((
         ans.pop_back();
     }
+    //Если баланс больше нуля - значит можно добавить закрывающуюся скобку;
     if (balance>0){
         ans.push_back(')');
         generate(pos+1,balance-1,ans);
